0ce0432d8fa08853c7e292fc9a3c4fd572ef3df3
Laura Feier <feierlaura10@gmail.com>
2015-09-11 18:13:47 +0300

    DJango 1.8 upgrade: removed some django1.9 deprecation warnings

--- hubbubs/models.py
+++ hubbubs/models.py
@@ -4,6 +4,9 @@ from django.core.urlresolvers import reverse
 
 class Subscription(AbstractSubscription):
 
+    class Meta:
+        app_label = 'hubbubs'
+
     @property
     def callback_url(self):
         return reverse('hubbubs_callback', args=[self.id, ])
